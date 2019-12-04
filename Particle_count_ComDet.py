465a17a1b18d7f29669283de6a43ae2d615d3713
Eric-Kobayashi <33022148+Eric-Kobayashi@users.noreply.github.com>
2019-12-04 17:12:34 +0000

    Update Particle_count_ComDet.py

--- Particle_count_ComDet.py
+++ Particle_count_ComDet.py
@@ -4,7 +4,7 @@ from ij import IJ
 from ij import WindowManager as wm
 import time
 
-pa = "/Volumes/GoogleDrive/My Drive/Aamir/Aamir_McEwan Lab_GDrive/Lab Book - Aamir/Takeda_DDU/July 2019/SHSY5Y Electroporation/Raw Data 1_4 days/Day 3"
+pa = r"E:\Dropbox (Cambridge University)\Artemisia\Aamir\Day3-DAPI\Tau"
 ch1a_arr = [3]
 ch1s_arr = [40]
 
@@ -15,53 +15,51 @@ def analysis(pa, ch1a, ch1s):
 	try:
 		for roots, dirs, files in os.walk(pa):
 			for f in files:
-				if f.endswith('.tif'):
+				if f.endswith('.tif') and 'FITC' in f:
 					List_of_images.append(op.join(roots, f))
 	except:
 		List_of_images = wm.getImageTitles()
 		flag = True
 							
-	List_of_results = []
 	for tit in List_of_images:
 		IJ.run("Clear Results")
 		try:
 			if flag:
 				imp = wm.getImage(tit)
+				t = tit
 			else:
 				imp = IJ.openImage(tit)
+				t = op.basename(tit)
 		except:
 			print('No images detected!')
 			continue
-
+#		t = t.replace(" ", "_")
+		result_path = op.join(pa, "summary.csv")
 		try:
 			IJ.redirectErrorMessages()
 			IJ.run(imp, "Detect Particles", "ch1a={} ch1s={} add=Nothing".format(ch1a, ch1s))
 		except:
-			with open(op.join(pa, "summary.csv"), 'a') as s:
-				contents = "0,0,0,0\n"
-				s.write("{},{},{},{}".format(tit.replace(".tif", ""), ch1a, ch1s, contents))
-			imp.close()
-			continue
-		
-		IJ.selectWindow("Summary")
-		result_path = op.join(pa, tit.replace(".tif", ".txt"))
-		IJ.saveAs("text", result_path)
-		List_of_results.append(result_path)
+			with open(op.join(pa, "summary.csv"), 'w') as s:
+				contents = "0\n"
+				s.write("{},{},{},{}".format(t.replace(".tif", ""), ch1a, ch1s, contents))
+		else:
+			IJ.selectWindow("Summary")
+			IJ.saveAs("text", result_path)
 
 		with open(op.join(pa, "particle_counts.csv"), 'a') as s:
-			with open(pa, 'r') as rf:
+			with open(result_path, 'r') as rf:
 				for lines in rf:
 					if 'Channel' not in lines:
-						contents = ','.join(lines.split('\t'))
-			s.write("{},{},{},{}".format(tit.replace(".tif", ""), ch1a, ch1s, contents))
+						contents = lines.split(',')[-1]
+			s.write("{},{},{},{}".format(t.replace(".tif", ""), ch1a, ch1s, contents))
 		imp.close()
-		
-	for r in List_of_results:
-		os.remove(r)
+
+	os.remove(result_path)
 		
 if __name__ in ["__main__", "__builtin__"]:
+	IJ.run("Input/Output...", "jpeg=85 gif=-1 file=.csv use_file copy_column save_column save_row")
 	with open(op.join(pa, "particle_counts.csv"), 'w') as s:
-		s.write("Image,ch1a,ch1s,Channel,Slice,Frame,Number_of_Particles\n")
+		s.write("Image,ch1a,ch1s,Number_of_Particles\n")
 	for ch1a in ch1a_arr:
 		for ch1s in ch1s_arr:
 			analysis(pa, ch1a, ch1s)
